bd8778fbc1b585e53fe7bd5f60fb474b70ac24f1
Bertie Raffle <bertierwork@gmail.com>
2024-10-21 16:11:44 +0100

    link server

--- client/app.js
+++ client/app.js
@@ -1,7 +1,9 @@
 const h1 = document.createElement("h1");
 
 async function changeH1() {
-  const response = await fetch("");
+  const response = await fetch(
+    "https://vite-test-pushing-from-local.onrender.com/"
+  );
   const text = await response.json;
   h1.textContent = text;
   document.body.appendChild(h1);
