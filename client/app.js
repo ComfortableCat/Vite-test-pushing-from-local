82dfe84576918658ffa821fafebc886ea5522cbd
Bertie Raffle <bertierwork@gmail.com>
2024-10-22 09:07:21 +0100

    fix?

--- client/app.js
+++ client/app.js
@@ -1,10 +1,11 @@
 const h1 = document.createElement("h1");
-
+console.log("fresh");
 async function changeH1() {
   const response = await fetch(
     "https://vite-test-pushing-from-local.onrender.com/"
   );
   const text = await response.json;
-  h1.textContent = text["message"];
+  h1.textContent = await text["message"];
+  console.log(test);
   document.body.appendChild(h1);
 }
